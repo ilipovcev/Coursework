575dd04685faa2d915081b336b38c7fca610814f
ilipovcev <ilya.lipovcev@bk.ru>
2020-12-12 22:26:39 +0300

    env

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const bodyParser = require('body-parser');
 const compression = require('compression');
-const keys = require('./keys');
 const postRouter = require('./routs/post');
 const authRouter = require('./routs/auth');
 const homeRouter = require('./routs/home');
 const loadPersonalRouter = require('./routs/loadPersonal');
 const personalRouter = require('./routs/personal');
 const path = require('path');
-const app = express(keys.mongoURL);
+const app = express();
 const port = process.env.PORT || 5000;
 const clientPath = path.join(__dirname, 'client');
 const exphbs = require('express-handlebars');
@@ -19,6 +18,7 @@ const MongoBlog = require('connect-mongodb-session')(session);
 const varMiddleware = require('./middleware/variables');
 const userMiddleware = require('./middleware/user');
 const errorMiddleware = require('./middleware/error');
+require('dotenv').config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -31,7 +31,7 @@ const hbs = exphbs.create({
 
 const blog = new MongoBlog({
   collection: 'sessions',
-  uri: keys.MONGODB_URI,
+  uri: process.env.MONGODB_URI,
 });
 
 app.engine('hbs', hbs.engine);
@@ -43,7 +43,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(
   session({
-    secret: keys.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     store: blog,
@@ -65,7 +65,7 @@ app.use(errorMiddleware);
 
 async function start() {
   try {
-    await mongoose.connect(keys.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
